303fb37601aba85ac137fd1a4cd8cbf57833142e
이재명 <jame0313@hanyang.ac.kr>
2021-09-24 07:45:49 +0000

    Make pokemon doesn't have evolution not printed

--- project1/36.sql
+++ project1/36.sql
@@ -1,5 +1,6 @@
 SELECT Pokemon.name
 From Pokemon
-LEFT OUTER JOIN Evolution ON Evolution.before_id = Pokemon.id
-WHERE Pokemon.type = 'Water' AND Evolution.after_id IS NULL
+JOIN Evolution AS First_Evolution ON First_Evolution.after_id = Pokemon.id
+LEFT OUTER JOIN Evolution AS Second_Evolution ON Second_Evolution.before_id = Pokemon.id
+WHERE Pokemon.type = 'Water' AND Second_Evolution.after_id IS NULL
 ORDER BY Pokemon.name;
