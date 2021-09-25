32783e75a1d107ba7fa21ab3e5e60df175624460
이재명 <jame0313@hanyang.ac.kr>
2021-09-25 06:19:33 +0000

    ADD GROUP BY to remove duplication

--- project1/33.sql
+++ project1/33.sql
@@ -2,4 +2,5 @@ SELECT Trainer.name
 FROM Trainer
 JOIN CatchedPokemon ON Trainer.id = CatchedPokemon.owner_id
 JOIN Pokemon ON Pokemon.type ='Psychic' AND CatchedPokemon.pid = Pokemon.id
+GROUP BY Trainer.id, Trainer.name
 ORDER BY Trainer.name;
