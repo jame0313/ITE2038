5262801e3c786eb3b066f3401668b3cf74370c73
이재명 <jame0313@hanyang.ac.kr>
2021-09-25 05:10:38 +0000

    Change SUBSTR to LEFT

--- project1/24.sql
+++ project1/24.sql
@@ -1,4 +1,4 @@
 SELECT Pokemon.name
 FROM Pokemon
-WHERE SUBSTR(Pokemon.name,1,1) IN ('A','E','I','O','U','a','e','i','o','u')
+WHERE LEFT(Pokemon.name,1) IN ('A','E','I','O','U','a','e','i','o','u')
 ORDER BY Pokemon.name;
