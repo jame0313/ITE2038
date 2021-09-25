2d344b4c68bdee172404e1f1205ab42644b99245
이재명 <jame0313@hanyang.ac.kr>
2021-09-25 03:12:14 +0000

    remove unnecessary join

--- project1/07.sql
+++ project1/07.sql
@@ -1,10 +1,9 @@
 SELECT CatchedPokemon.nickname
 FROM
-(SELECT City.name AS name, MAX(CatchedPokemon.level) AS maxlevel
+(SELECT Trainer.hometown AS name, MAX(CatchedPokemon.level) AS maxlevel
 FROM CatchedPokemon
 JOIN Trainer ON Trainer.id = CatchedPokemon.owner_id
-JOIN City ON City.name = Trainer.hometown
-GROUP BY City.name) AS CityMaxLevel
+GROUP BY Trainer.hometown) AS CityMaxLevel
 JOIN Trainer ON Trainer.hometown = CityMaxLevel.name
 JOIN CatchedPokemon ON Trainer.id = CatchedPokemon.owner_id AND CatchedPokemon.level = CityMaxLevel.maxlevel
 ORDER BY CatchedPokemon.nickname;
