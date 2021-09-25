61eda1abd58b6549364dab0d0c621fa9cf6b1a55
이재명 <jame0313@hanyang.ac.kr>
2021-09-25 05:37:06 +0000

    edit typo

--- project1/25.sql
+++ project1/25.sql
@@ -2,5 +2,5 @@ SELECT Second_Pokemon.name
 FROM Pokemon AS Second_Pokemon
 JOIN Evolution AS First_Evolution ON Second_Pokemon.id = First_Evolution.after_id
 LEFT OUTER JOIN Evolution AS NULL_Evolution ON First_Evolution.before_id = NULL_Evolution.after_id
-WHERE NULL_Evolution.after_id is NULL
+WHERE NULL_Evolution.after_id IS NULL
 ORDER BY Second_Pokemon.name;
