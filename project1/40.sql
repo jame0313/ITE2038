b77755b7c51477e1ad95bb7ded5c016b5bd7b7f2
이재명 <jame0313@hanyang.ac.kr>
2021-09-25 06:35:05 +0000

    Add GROUP BY to handle namesake

--- project1/40.sql
+++ project1/40.sql
@@ -1,5 +1,6 @@
-SELECT DISTINCT Trainer.name
+SELECT Trainer.name
 From Trainer
 JOIN CatchedPokemon ON Trainer.id = CatchedPokemon.owner_id
 JOIN Pokemon ON Pokemon.name = 'Pikachu' AND CatchedPokemon.pid = Pokemon.id
+GROUP BY Trainer.id, Trainer.name
 ORDER BY Trainer.name;
