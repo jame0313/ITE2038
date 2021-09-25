b6b260cb99ac63e58fb6e84f74b044ec48571d96
이재명 <jame0313@hanyang.ac.kr>
2021-09-25 05:07:20 +0000

    add nested query to handle namesake

--- project1/21.sql
+++ project1/21.sql
@@ -1,6 +1,7 @@
-SELECT DISTINCT Trainer.name
+SELECT Duplicated_Trainer.name
+FROM (SELECT DISTINCT Trainer.id AS id, Trainer.name AS name
 FROM Trainer
 JOIN CatchedPokemon ON Trainer.id = CatchedPokemon.owner_id
 GROUP BY Trainer.id, Trainer.name, CatchedPokemon.pid
-HAVING COUNT(*) >= 2
-ORDER BY Trainer.name;
+HAVING COUNT(*) >= 2) AS Duplicated_Trainer
+ORDER BY Duplicated_Trainer.name;
