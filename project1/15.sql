a9cd608d1cb8702c69fc92895171439e75621cee
이재명 <jame0313@hanyang.ac.kr>
2021-09-25 04:11:50 +0000

    Edit typo

--- project1/15.sql
+++ project1/15.sql
@@ -1,5 +1,5 @@
 SELECT DISTINCT Trainer.name
 From Pokemon
 JOIN CatchedPokemon ON Pokemon.name LIKE 'P%' AND Pokemon.id = CatchedPokemon.pid
-JOIN Trainer ON Trainer.hometown = 'Sangnok city' AND Trainer.id = CatchedPokemon.owner_id
+JOIN Trainer ON Trainer.hometown = 'Sangnok City' AND Trainer.id = CatchedPokemon.owner_id
 ORDER BY Trainer.name;
