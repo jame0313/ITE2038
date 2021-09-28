67df7ffad7edd4595656faf6620fd9457675f8e7
2018008940 <jame0313@hanyang.ac.kr>
2021-09-29 02:03:18 +0900

    change c-style io to linux sys call and make testcode

--- project2/googletest/basic_test.cc
+++ project2/googletest/basic_test.cc
@@ -1,22 +1,81 @@
 #include <gtest/gtest.h>
-#include <cmath>
+#include "file.h"
 #include <vector>
 
-int Factorial(int n){
-    if(n==0) return 1;
-    int ret = 1;
-    for(int i=2;i<=n;i++) ret *= i;
-    return ret;
+TEST(DiskSpaceManager, FileInitialization){
+    const char* path = "./FileInitialization.db";
+    int fd = file_open_database_file(path);
+    
+    loff_t siz = lseek64(fd,0,SEEK_END);
+    EXPECT_EQ(siz, 1024*1024*10);
+
+    page_t header_page;
+    file_read_page(fd,0,&header_page);
+    uint64_t num_of_page = DSM::read_data_from_page(&header_page,NUMBER_OF_PAGES_OFFSET);
+    EXPECT_EQ(num_of_page,DEFAULT_PAGE_NUMBER);
+
+    file_close_database_file();
+    remove(path);
 }
 
-bool IsPrime(int n){
-    if(n<2) return false;
-    for(int i=2;i<=sqrt(n);i++){
-        if(n%i==0) return false;
-    }
-    return true;
+TEST(DiskSpaceManager, PageManagement){
+    const char* path = "./PageManagement.db";
+    int fd = file_open_database_file(path);
+    
+    pagenum_t page1, page2;
+    page1 = file_alloc_page(fd);
+    page2 = file_alloc_page(fd);
+    EXPECT_GT(page1,0);
+    EXPECT_GT(page2,0);
+
+    file_free_page(fd,page2);
+
+    pagenum_t pn = 0;
+    page_t tmp;
+    std::vector<pagenum_t> list;
+    bool hasfreedpage = false;
+    do{
+        list.push_back(pn);
+        file_read_page(fd,pn,&tmp);
+        pagenum_t np = DSM::read_data_from_page(&tmp,PAGE_NUMBER_OFFSET);
+        EXPECT_NE(page1, pn);
+        hasfreedpage |= pn == page2;
+        pn = np;
+    }while(pn);
+    EXPECT_TRUE(hasfreedpage);
+
+    page_t header_page;
+    file_read_page(fd,0,&header_page);
+    uint64_t num_of_page = DSM::read_data_from_page(&header_page,NUMBER_OF_PAGES_OFFSET);
+    EXPECT_EQ(num_of_page,list.size()+1);
+
+    list.clear();
+    file_close_database_file();
+    remove(path);
 }
 
-TEST(FactorialTest, HandlesZeroInput){
-    EXPECT_EQ(Factorial(0), 1);
+TEST(DiskSpaceManager, PageIO){
+    const char* path = "./PageIO.db";
+    int fd = file_open_database_file(path);
+    
+    pagenum_t pg;
+    pg = file_alloc_page(fd);
+    
+    EXPECT_GT(pg,0);
+
+    page_t sample, target;
+
+    char * str = new char[4096];
+    for(int i=0;i<4096;i++) str[i] = 'X';
+    memcpy(&sample,str,4096);
+
+    file_write_page(fd,pg,&sample);
+    file_read_page(fd,pg,&target);
+
+    EXPECT_EQ(memcmp(&sample,&target,4096),0);
+
+    file_free_page(fd,pg);
+
+    file_close_database_file();
+    remove(path);
 }
--- project2/src/file.cpp
+++ project2/src/file.cpp
@@ -1,131 +1,167 @@
 #include "file.h"
 
 namespace DSM{
+    
+    std::set<int> DB_FILE_SET;
 
-    void init_header_page(page_t* pg, pagenum_t nxt_page_number,  int64_t number_of_pages){
-        memset(pg, 0, sizeof page_t);
+    bool is_file_opened(int fd){
+        return DB_FILE_SET.find(fd)!=DB_FILE_SET.end();
+    }
+
+    bool is_pagenum_valid(int fd, pagenum_t pagenum){
+        if(!pagenum) return true;
+        page_t header_page;
+        load_page_from_file(fd,0,&header_page);
+        uint64_t number_of_pages = read_data_from_page(&header_page,NUMBER_OF_PAGES_OFFSET);
+        return pagenum < number_of_pages;
+    }
+
+    
+    void init_header_page(page_t* pg, pagenum_t nxt_page_number,  uint64_t number_of_pages){
+        memset(pg, 0, sizeof(page_t));
         *reinterpret_cast<pagenum_t*>(pg->raw_data + PAGE_NUMBER_OFFSET) = nxt_page_number;
-        *reinterpret_cast<int64_t*>(pg->raw_data + NUMBER_OF_PAGES_OFFSET) = number_of_pages;
+        *reinterpret_cast<uint64_t*>(pg->raw_data + NUMBER_OF_PAGES_OFFSET) = number_of_pages;
     }
 
     void init_free_page(page_t* pg, pagenum_t nxt_page_number){
-        memset(pg, 0, sizeof page_t);
+        memset(pg, 0, sizeof(page_t));
         *reinterpret_cast<pagenum_t*>(pg->raw_data + PAGE_NUMBER_OFFSET) = nxt_page_number;
+    }
+
+    uint64_t read_data_from_page(const page_t* pg, uint64_t offset){
+        return *reinterpret_cast<const uint64_t*>(pg->raw_data + offset);
+    }
+
+    void write_data_from_page(page_t* pg, uint64_t offset, uint64_t val){
+        *reinterpret_cast<uint64_t*>(pg->raw_data + offset) = val;
+    }
+
+    void store_page_to_file(int fd, pagenum_t pagenum, const page_t* src){
+        pwrite64(fd,src,sizeof(page_t),pagenum*PAGE_SIZE);
+        fsync(fd);
+    }
+
+    void load_page_from_file(int fd, pagenum_t pagenum, page_t* dest){
+        pread64(fd,dest,sizeof(page_t),pagenum*PAGE_SIZE);
     }
 }
 
 
-int64_t file_open_database_file(char* path){
-    FILE* fp = fopen(path,"rb+");
+int file_open_database_file(const char* path){
+    
+    int fd;
 
-    if(!fp){
-        fp = fopen(path,"wb+");
+    if((fd=open64(path, O_RDWR)) == -1){
+        if((fd=open64(path, O_RDWR | O_CREAT, 0644)) == -1){
+            throw "file_open_database_file failed";
+        }
+
         page_t header_page, free_page;
 
         DSM::init_header_page(&header_page, 1, DEFAULT_PAGE_NUMBER);
-        fwrite(&header_page, sizeof page_t, 1, fp);
+        DSM::store_page_to_file(fd, 0, &header_page);
 
         for(int i=1;i<DEFAULT_PAGE_NUMBER;i++){
             DSM::init_free_page(&free_page, i+1>=DEFAULT_PAGE_NUMBER?0:i+1);
-            fwrite(&free_page, sizeof page_t, 1, fp);
+            DSM::store_page_to_file(fd, i, &free_page);
         }
-
-        fflush(fp);
     }
 
-    int64_t return_id = DSM::DB_FILE_SIZE;
-    DSM::DB_FILE_MAP[DSM::DB_FILE_SIZE++] = fp;
+    DSM::DB_FILE_SET.insert(fd);
     
-    return return_id;
+    return fd;
 }
 
-pagenum_t file_alloc_page(){
-    if(DSM::DB_FILE_SIZE<=0){
-        throw "you should call file_open_database_file first!";
+pagenum_t file_alloc_page(int fd){
+    if(!DSM::is_file_opened(fd)){
+        throw "unvalid file descriptor";
     }
     
     page_t header_page;
-    file_read_page(0, &header_page);
-    pagenum_t &nxt_page_number = *reinterpret_cast<pagenum_t*>(header_page.raw_data + PAGE_NUMBER_OFFSET);
+
+    DSM::load_page_from_file(fd,0,&header_page);
+
+    uint64_t nxt_page_number = DSM::read_data_from_page(&header_page,PAGE_NUMBER_OFFSET);
     
     if(nxt_page_number){
         page_t nxt_page;
-        file_read_page(nxt_page_number, &nxt_page);
-        nxt_page_number = *reinterpret_cast<pagenum_t*>(nxt_page.raw_data + PAGE_NUMBER_OFFSET);
-        file_write_page(0, &header_page);
-        return nxt_page_number;
+        DSM::load_page_from_file(fd,nxt_page_number,&nxt_page);
+
+        uint64_t nxt_nxt_page_number = DSM::read_data_from_page(&nxt_page,PAGE_NUMBER_OFFSET);
+        DSM::write_data_from_page(&header_page,PAGE_NUMBER_OFFSET,nxt_nxt_page_number);
+        DSM::init_free_page(&nxt_page,0);
+
+        DSM::store_page_to_file(fd,0,&header_page);
+        DSM::store_page_to_file(fd,nxt_page_number,&nxt_page);
     }
     else{
-        int64_t &current_number_of_pages = *reinterpret_cast<int64_t*>(header_page.raw_data + NUMBER_OF_PAGES_OFFSET);
+        uint64_t current_number_of_pages = DSM::read_data_from_page(&header_page,NUMBER_OF_PAGES_OFFSET);
         size_t num_of_new_pages = current_number_of_pages;
 
         nxt_page_number = current_number_of_pages;
-        current_number_of_pages += num_of_new_pages;
 
-        file_write_page(0, &header_page);
-
-        FILE* fp = DSM::DB_FILE_MAP.rbegin()->second;
-        fseek(fp, 0, SEEK_END);
+        DSM::init_header_page(&header_page, current_number_of_pages+1, current_number_of_pages<<1);
+        DSM::store_page_to_file(fd,0,&header_page);
 
         page_t free_page;
 
-        for(int i=0;i<num_of_new_pages;i++){
-            DSM::init_free_page(&free_page, current_number_of_pages + (i+1>=DEFAULT_PAGE_NUMBER?0:i+1));
-            fwrite(&free_page, sizeof page_t, 1, fp);
+        for(uint64_t i=0;i<num_of_new_pages;i++){
+            DSM::init_free_page(&free_page, current_number_of_pages + (i==0||i+1>=current_number_of_pages?0:i+1));
+            DSM::store_page_to_file(fd,current_number_of_pages + i,&free_page);
         }
 
-        fflush(fp);
     }
+
+    return nxt_page_number;
 }
 
-void file_free_page(pagenum_t pagenum){
-    if(DSM::DB_FILE_SIZE<=0){
-        throw "you should call file_open_database_file first!";
+void file_free_page(int fd, pagenum_t pagenum){
+    if(!DSM::is_file_opened(fd)){
+        throw "unvalid file descriptor";
+    }
+    if(!DSM::is_pagenum_valid(fd,pagenum)){
+        throw "pagenum is out of bound in fie_read_page";
     }
 
     page_t header_page, new_page;
 
-    file_read_page(0, &header_page);
-    file_read_page(pagenum, &new_page);
+    DSM::load_page_from_file(fd,0,&header_page);
+    DSM::load_page_from_file(fd,pagenum,&new_page);
+
+    uint64_t header_page_nxt_page = DSM::read_data_from_page(&header_page,PAGE_NUMBER_OFFSET);
+    DSM::init_free_page(&new_page,header_page_nxt_page);
+    DSM::write_data_from_page(&header_page,PAGE_NUMBER_OFFSET,pagenum);
     
-    *reinterpret_cast<pagenum_t*>(new_page.raw_data + PAGE_NUMBER_OFFSET) = *reinterpret_cast<pagenum_t*>(header_page.raw_data + PAGE_NUMBER_OFFSET);
-    *reinterpret_cast<pagenum_t*>(header_page.raw_data + PAGE_NUMBER_OFFSET) = pagenum;
-
-    file_write_page(0, &header_page);
-    file_write_page(pagenum, &new_page);
+    
+    DSM::store_page_to_file(fd,0,&header_page);
+    DSM::store_page_to_file(fd,pagenum,&new_page);
 }
 
-void file_read_page(pagenum_t pagenum, page_t* dest){
-    if(DSM::DB_FILE_SIZE<=0){
-        throw "you should call file_open_database_file first!";
+void file_read_page(int fd, pagenum_t pagenum, page_t* dest){
+    if(!DSM::is_file_opened(fd)){
+        throw "unvalid file descriptor";
     }
-
-    FILE* fp = DSM::DB_FILE_MAP.rbegin()->second;
-    fseek(fp, PAGE_SIZE*pagenum, SEEK_SET);
-
-    fread(dest, sizeof page_t, 1, fp);
+    if(!DSM::is_pagenum_valid(fd,pagenum)){
+        throw "pagenum is out of bound in file_read_page";
+    }
+    DSM::load_page_from_file(fd,pagenum,dest);
 }
 
-void file_write_page(pagenum_t pagenum, const page_t* src){
-    if(DSM::DB_FILE_SIZE<=0){
-        throw "you should call file_open_database_file first!";
+void file_write_page(int fd, pagenum_t pagenum, const page_t* src){
+    if(!DSM::is_file_opened(fd)){
+        throw "unvalid file descriptor";
+    }
+    if(!DSM::is_pagenum_valid(fd,pagenum)){
+        throw "pagenum is out of bound in file_write_page";
     }
 
-    FILE* fp = DSM::DB_FILE_MAP.rbegin()->second;
-    fseek(fp, PAGE_SIZE*pagenum, SEEK_SET);
-
-    fwrite(src, sizeof page_t, 1, fp);
-    fflush(fp);
+    DSM::store_page_to_file(fd,pagenum,src);
+    
 }
 
 void file_close_database_file(){
-    if(DSM::DB_FILE_SIZE<=0){
-        throw "you should call file_open_database_file first!";
-    }
+    
+    for(int fd : DSM::DB_FILE_SET) close(fd);
 
-    for(auto& it : DSM::DB_FILE_MAP){
-        if(it.second) fclose(it.second);
-    }
-
-    DSM::DB_FILE_MAP.clear();
+    DSM::DB_FILE_SET.clear();
 }
