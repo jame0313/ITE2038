0118673c3e16ffadc5d61369cfe6e866633dc66f
2018008940 <jame0313@hanyang.ac.kr>
2021-11-01 01:31:49 +0900

    fix wildcard api

--- project3/db_project/db/src/wildcard.cc
+++ project3/db_project/db/src/wildcard.cc
@@ -1,12 +1,11 @@
 #include "wildcard.h"
 
-page_t get_header_page_from_multiple_layer(int64_t table_id){
-    page_t ret_page;
-    memset(&ret_page, 0, sizeof(page_t));
-    buffer_read_page(table_id, 0, &ret_page);
-    return ret_page;
+void get_header_page_from_multiple_layer(int64_t table_id, page_t* dest){
+    return buffer_read_page(table_id, 0, dest);
 }
 
 void set_header_page_from_multiple_layer(int64_t table_id, const page_t* src){
     buffer_write_page(table_id, 0, src);
+    file_write_page(table_id, 0, src);
+    return;
 }
