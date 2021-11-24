396313aa65c3adeb6850d009415f3d79f1497c99
2018008940 <jame0313@hanyang.ac.kr>
2021-11-24 19:24:57 +0900

    add idx with txn and trx api

--- project5/db_project/db/include/api.h
+++ project5/db_project/db/include/api.h
@@ -29,3 +29,26 @@ int init_db(int num_buf = DEFAULT_BUFFER_SIZE);
 //Shutdown your database management system
 //If success, return 0. Otherwise, return non zero value.
 int shutdown_db();
+
+//Read a value in the table with a matching key for the transaction having trx_id
+//return 0 (SUCCESS): operation is successfully done, and the transaction can continue the next operation.
+//return non zero (FAILED): operation is failed (e.g., deadlock detected), and the transaction should be
+//aborted. Note that all tasks that need to be handled should be completed in db_find
+int db_find(int64_t table_id, int64_t key, char *ret_val, uint16_t *val_size, int trx_id);
+
+//Find the matching key and modify the values
+//If found matching key, update the value of the record to values string with its new_val_size and store its size in old_val_size
+//return 0 (SUCCESS): operation is successfully done, and the transaction can continue the next operation.
+//return non zero (FAILED): operation is failed (e.g., deadlock detected), and the transaction should be aborted.
+//Note that all tasks that need to be handled should be completed in db_update
+int db_update(int64_t table_id, int64_t key, char *values, uint16_t new_val_size, uint16_t *old_val_size, int trx_id);
+
+//Allocate a transaction structure and initialize it.
+//Return a unique transaction id (>= 1) if success, otherwise return 0.
+int trx_begin(void);
+
+//Clean up the transaction with the given trx_id (transaction id) and its related information that has
+//been used in your lock manager
+//Shrinking phase of strict 2PL
+//Return the completed transaction id if success, otherwise return 0.
+int trx_commit(int trx_id);
--- project5/db_project/db/src/api.cc
+++ project5/db_project/db/src/api.cc
@@ -6,6 +6,8 @@ int init_db(int num_buf){
 
 int shutdown_db(){
     try{
+        close_trx_manager();
+        close_lock_table();
         buffer_close_table_file();
         file_close_table_file();
         return 0;
@@ -35,4 +37,20 @@ int db_find(int64_t table_id, int64_t key, char *ret_val, uint16_t *val_size){
 
 int db_delete(int64_t table_id, int64_t key){
     return idx_delete_by_key(table_id, key);
+}
+
+int db_find(int64_t table_id, int64_t key, char *ret_val, uint16_t *val_size, int trx_id){
+    return idx_find_by_key_trx(table_id, key, ret_val, val_size, trx_id);
+}
+
+int db_update(int64_t table_id, int64_t key, char *values, uint16_t new_val_size, uint16_t *old_val_size, int trx_id){
+    return idx_update_by_key_trx(table_id, key, values, new_val_size, old_val_size, trx_id);
+}
+
+int trx_begin(void){
+    return trx_begin_txn();
+}
+
+int trx_commit(int trx_id){
+    return trx_commit_txn(trx_id);
 }
